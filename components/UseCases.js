407c15a62d90c9a1b3b1edf5bbca2b6e6a4ffe4a
Muhammad Awais Abdullah <143254491+Awadul@users.noreply.github.com>
2025-08-29 22:47:47 +0500

    fixed the cases section for desktop

--- components/UseCases.js
+++ components/UseCases.js
@@ -3,31 +3,32 @@ export function UseCases() {
         <section class="use-cases" id="use-case">
             <div class="container">
                 <div class="use-cases-header">
+                    <p class="use-cases-badge">USE CASE</p>
                     <h2 class="section-title use-cases-title">事例</h2>
-                    <div class="use-cases-badge">USE CASE</div>
                 </div>
                 
                 <div class="use-cases-list">
+                
                     <!-- Case 01 -->
-                    <div class="case-item case-01">
+                    <div class="case-item case">
                         <h3 class="case-title"><span class="case-number">Case 01</span><span class="case-title-rest">｜社内にM&A人材がいない中で、初の買収を実現</span></h3>
                         
-                        <div class="case-01-challenges">
-                            <div class="case-01-head">課題</div>
-                            <div class="case-01-row">
+                        <div class="case-challenges">
+                            <div class="case-head">課題</div>
+                            <div class="case-row">
                                 <img src="images/case01-challenge.png" alt="課題イラスト" class="challenge-illustration" />
                                 <div class="challenge-pills">
-                                    <div class="pill">M&Aによる非連続成長を検討していたが、<br>社内に専任担当者がいなかった。</div>
-                                    <div class="pill">仲介会社から紹介される案件も少なく、<br>進め方も不明瞭だった。</div>
+                                    <div class="pill">M&Aによる非連続成長を検討していたが、</br>社内に専任担当者がいなかった。</div>
+                                    <div class="pill">仲介会社から紹介される案件も少なく、</br>進め方も不明瞭だった。</div>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="case-01-support">
-                            <div class="case-01-support-arrow-container">
-                                <img src="images/arrow.png" alt="下向きドット矢印" class="case-01-support-arrow" />
+                        <div class="case-support">
+                            <div class="case-support-arrow-container">
+                                <img src="images/arrow.png" alt="下向きドット矢印" class="case-support-arrow" />
                             </div>
-                            <div class="case-01-support-pill-container">
+                            <div class="case-support-pill-container">
                                 <div class="support-box one-row">
                                     <ul class="support-list">
                                         <li>投資方針・ターゲット要件の整理</li>
@@ -38,23 +39,23 @@ export function UseCases() {
                             </div>
                         </div>
 
-                        <div class="case-01-result">
-                            <div class="case-01-result-layout">
-                                <img src="images/case01-result.png" alt="Case 01 Result" class="case-01-result-illustration" />
+                        <div class="case-result">
+                            <div class="case-result-layout">
+                                <img src="images/case01-result.png" alt="Case 02 Result" class="case-result-illustration" />
                                 <div class="result-pill">
-                                    数ヶ月以内に理想に近い企業とのトップ面談が実現。<br>買収契約締結後も、事業統合と営業支援を継続的に実施。
+                                    新規事業が2ヶ月でローンチ、営業成果も改善。<br>組織の士気向上と定着率アップにも貢献。
                                 </div>
                             </div>
                         </div>
                     </div>
                     
                     <!-- Case 02 -->
-                    <div class="case-item case-01">
+                    <div class="case-item case">
                         <h3 class="case-title"><span class="case-number">Case 02</span><span class="case-title-rest">｜"M&A後"に動けず苦戦。PMIと新規事業でバリューアップに成功</span></h3>
                         
-                        <div class="case-01-challenges">
-                            <div class="case-01-head">課題</div>
-                            <div class="case-01-row">
+                        <div class="case-challenges">
+                            <div class="case-head">課題</div>
+                            <div class="case-row">
                                 <img src="images/case01-challenge.png" alt="課題イラスト" class="challenge-illustration" />
                                 <div class="challenge-pills">
                                     <div class="pill">買収まではうまくいったが、M&A後に何をすればよいか<br>分からず、事業成長が停滞していた。</div>
@@ -63,11 +64,11 @@ export function UseCases() {
                             </div>
                         </div>
 
-                        <div class="case-01-support">
-                            <div class="case-01-support-arrow-container">
-                                <img src="images/arrow.png" alt="下向きドット矢印" class="case-01-support-arrow" />
+                        <div class="case-support">
+                            <div class="case-support-arrow-container">
+                                <img src="images/arrow.png" alt="下向きドット矢印" class="case-support-arrow" />
                             </div>
-                            <div class="case-01-support-pill-container">
+                            <div class="case-support-pill-container">
                                 <div class="support-box one-row">
                                     <ul class="support-list">
                                         <li>買収先の強みと課題を洗い出し、新規サービスの立案</li>
@@ -78,9 +79,9 @@ export function UseCases() {
                             </div>
                         </div>
 
-                        <div class="case-01-result">
-                            <div class="case-01-result-layout">
-                                <img src="images/case01-result.png" alt="Case 02 Result" class="case-01-result-illustration" />
+                        <div class="case-result">
+                            <div class="case-result-layout">
+                                <img src="images/case01-result.png" alt="Case 02 Result" class="case-result-illustration" />
                                 <div class="result-pill">
                                     新規事業が2ヶ月でローンチ、営業成果も改善。<br>組織の士気向上と定着率アップにも貢献。
                                 </div>
@@ -89,12 +90,12 @@ export function UseCases() {
                     </div>
                     
                     <!-- Case 03 -->
-                    <div class="case-item case-01">
+                    <div class="case-item case">
                         <h3 class="case-title"><span class="case-number">Case 03</span><span class="case-title-rest">｜仲介依存から脱却し、理想のターゲットを自主開拓</span></h3>
                         
-                        <div class="case-01-challenges">
-                            <div class="case-01-head">課題</div>
-                            <div class="case-01-row">
+                        <div class="case-challenges">
+                            <div class="case-head">課題</div>
+                            <div class="case-row">
                                 <img src="images/case01-challenge.png" alt="課題イラスト" class="challenge-illustration" />
                                 <div class="challenge-pills">
                                     <div class="pill">仲介会社からの紹介に限界を感じ、希望条件に合う案件と出会えなかった。</div>
@@ -103,11 +104,11 @@ export function UseCases() {
                             </div>
                         </div>
 
-                        <div class="case-01-support">
-                            <div class="case-01-support-arrow-container">
-                                <img src="images/arrow.png" alt="下向きドット矢印" class="case-01-support-arrow" />
+                        <div class="case-support">
+                            <div class="case-support-arrow-container">
+                                <img src="images/arrow.png" alt="下向きドット矢印" class="case-support-arrow" />
                             </div>
-                            <div class="case-01-support-pill-container">
+                            <div class="case-support-pill-container">
                                 <div class="support-box one-row">
                                     <ul class="support-list">
                                         <li>独自ソーシング戦略を策定</li>
@@ -118,9 +119,9 @@ export function UseCases() {
                             </div>
                         </div>
 
-                        <div class="case-01-result">
-                            <div class="case-01-result-layout">
-                                <img src="images/case01-result.png" alt="Case 03 Result" class="case-01-result-illustration" />
+                        <div class="case-result">
+                            <div class="case-result-layout">
+                                <img src="images/case01-result.png" alt="Case 03 Result" class="case-result-illustration" />
                                 <div class="result-pill">
                                     3ヶ月で6社との面談機会を獲得し、候補企業との実質的な交渉フェーズへ。<br>社内からの評価も高まり、経営陣主導のM&A体制が定着。
                                 </div>
